a103680def7097b301f5959ce8c552fdd8bb4360
alray <1780284652@qq.com>
2025-03-03 03:43:18 +0000

    Feat: transform 添加速度

--- Transform/Transform.cs
+++ Transform/Transform.cs
@@ -4,6 +4,7 @@ class Transform : Component
 {
     public Vector3d Position;
     public Quaterniond Rotation;
+    public Vector3d Velocity;
     // public Vector3d LocalPosition { get => Parent == null ? Position : Position - Parent.Position; }
     // public Quaterniond LocalRotation { get => Parent == null ? Rotation : Rotation * Parent.Rotation.Conjugate(); }
 
